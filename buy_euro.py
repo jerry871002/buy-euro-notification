97538e6027fa46b450c276f7d8a1e7d5767608f8
Jerry Yang <jerry871002@gmail.com>
2022-06-17 23:50:04 +0800

    The bank's website is updated

--- buy_euro.py
+++ buy_euro.py
@@ -1,11 +1,14 @@
+import json
 import requests
-import re
-from bs4 import BeautifulSoup
 from datetime import datetime
+from pathlib import Path
 
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s',
-                    filename='buy_euro_log.txt')
+logging.basicConfig(
+    level=logging.DEBUG, 
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename=Path(__file__).parent / 'buy_euro_log.txt'
+)
 
 API_KEY = 'your api key here'
 TARGET_PRICE = 31.5
@@ -18,14 +21,32 @@ def main():
 
     logging.info(euro_prices)
 
-    current_price = float(euro_prices[3])
+    if not euro_prices:
+        logging.warning("No data in the response.")
+        msg = (
+            "\n無法從網站上抓取資料\n"
+            f"更新時間： {current_time}"
+        )
+
+        response = post_to_line_chat(msg)
+        if response.status_code == 200:
+            logging.info("Message sent.")
+        else:
+            logging.error(f"Status code: {response.status_code}")
+            logging.error("Something went wrong with the LINE Notify service.")
+        
+        return 
+
+    current_price = float(euro_prices['spotExchangeRate']['sale'])
     previous_price = get_previous_price()
 
     if current_price <= TARGET_PRICE and current_price != previous_price:
-        msg = "\n快買歐元喔！\n" \
-             f"即期賣匯： {euro_prices[3]}\n" \
-             f"現金賣匯： {euro_prices[4]}\n" \
-             f"更新時間： {current_time}"
+        msg = (
+            "\n快買歐元喔！\n"
+            f"即期賣匯： {euro_prices['spotExchangeRate']['sale']}\n"
+            f"現金賣匯： {euro_prices['cashExchangeRate']['sale']}\n"
+            f"更新時間： {current_time}"
+        )
 
         response = post_to_line_chat(msg)
 
@@ -36,22 +57,19 @@ def main():
             logging.error("Something went wrong with the LINE Notify service.")
 
         # save current price to file
-        print(euro_prices[3], file=open('previous_price.txt', 'w'))
+        print(euro_prices['spotExchangeRate']['sale'], file=open('previous_price.txt', 'w'))
 
 def get_currency_list():
-    r = requests.get("https://wwwfile.megabank.com.tw/rates2/M001/viewF_new_02_02.asp")
-    c = r.content
-    soup = BeautifulSoup(c, "html.parser")
-    return soup.find_all("tr", {"class": re.compile("tbcolor[12]")})
+    r = requests.post("https://www.megabank.com.tw/api/sc/RateExchange/Get_Fx_Currency")
+    return json.loads(r.content.decode('utf-8'))['appRepBody']['exchangeRates']
 
 def get_euro_prices(currency_list):
-    result = []
     for item in currency_list:
-        currency = item.find_all("td")
-        if currency[0].text == "歐元[EUR]":
-            for i in range(len(currency)):
-                result.append(currency[i].text)
-    return result
+        if item['currency'] == 'EUR':
+            return item
+
+    # if something went wrong, return an empty dictionary
+    return {}
 
 def get_previous_price():
     try:
